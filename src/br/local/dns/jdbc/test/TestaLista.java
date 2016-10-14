d5b5818ed8ae135a2a8fa641f3f22403c4b59a3f
Daniel Santos <dns6666@gmail.com>
2016-10-14 12:31:59 -0300

    test home

---
+++ src/br/local/dns/jdbc/test/TestaLista.java
@@ -0,0 +1,26 @@
+package br.local.dns.jdbc.test;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import br.local.dns.jdbc.modelo.Contato;
+import br.local.dns.jdbc.modelo.dao.ContatoDao;
+
+public class TestaLista {
+	public static void main(final String[] args) {
+
+		final ContatoDao dao = new ContatoDao();
+		final List<Contato> contatos = dao.getListaPosicao((long) 2);
+
+		for (final Contato contato : contatos) {
+			System.out.println("Nome: " + contato.getName());
+			System.out.println("Email: " + contato.getEmail());
+			System.out.println("Endereço: " + contato.getAddress());
+			System.out.println("Data de Nascimento: " + contato.getBirthDate().getTime() + "\n");
+
+			final SimpleDateFormat simple = new SimpleDateFormat("dd/MM/yyyy");
+			System.out.println("Data de Nascimento: " + simple.format(contato.getBirthDate().getTime()) + "\n");
+
+		}
+	}
+}
